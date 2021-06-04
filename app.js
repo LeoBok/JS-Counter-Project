996b2c941197c981335c31b9f195c5cd5d27a596
Leonardo Boccali <leonardoboccali19@gmail.com>
2021-06-04 12:47:51 +0200

    selected the buttons (of the HTML file) in the JS file with document.getElementById()

---
+++ app.js
@@ -0,0 +1,5 @@
+let counterNum = document.getElementById("num-showed");
+let add = document.getElementById("add-btn");
+let subtract = document.getElementById("subtract-btn");
+let resetBtn = document.getElementById("reset-btn");
+let magicBtn = document.getElementById("magicButton-btn");

d6d6ad40a250da22961261a9753def5cf4f2ee2c
Leonardo Boccali <leonardoboccali19@gmail.com>
2021-06-04 12:59:40 +0200

    added an event listener to the 'resetBtn button' for reset the counter to 0

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let counterNum = document.getElementById("num-showed");
+
 let add = document.getElementById("add-btn");
 let subtract = document.getElementById("subtract-btn");
 let resetBtn = document.getElementById("reset-btn");
@@ -9,9 +10,14 @@ let counter = 0;
 add.addEventListener("click", function() {
     counter ++;
     counterNum.innerText = counter;
-})
+});
 
 subtract.addEventListener("click", function() {
     counter --;
+    counterNum.innerText = counter;
+});
+
+resetBtn.addEventListener("click", function() {
+    counter = 0;
     counterNum.innerText = counter;
 })

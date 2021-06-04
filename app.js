d758aa2b77875f9ecc3bce075a9c8e0d2470f475
Leonardo Boccali <leonardoboccali19@gmail.com>
2021-06-04 12:50:50 +0200

    created a variable called 'counter' in JS file

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ let add = document.getElementById("add-btn");
 let subtract = document.getElementById("subtract-btn");
 let resetBtn = document.getElementById("reset-btn");
 let magicBtn = document.getElementById("magicButton-btn");
+
+let counter = 0;

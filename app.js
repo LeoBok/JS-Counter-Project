e011409045ac3005b3fa37190ed082cf1e75ba48
Leonardo Boccali <leonardoboccali19@gmail.com>
2021-06-06 20:15:08 +0200

    created a function called 'changeColor()' wich has to change the color of the shown number to green, if is positive, or red, if is negative. For this I have to create two class in JS with the classList.add() command and connect them in the css file with the respective class names

--- app.js
+++ app.js
@@ -10,14 +10,36 @@ let counter = 0;
 add.addEventListener("click", function() {
     counter ++;
     counterNum.innerText = counter;
+    changeColor();
 });
 
 subtract.addEventListener("click", function() {
     counter --;
     counterNum.innerText = counter;
+    changeColor();
 });
 
 resetBtn.addEventListener("click", function() {
     counter = 0;
     counterNum.innerText = counter;
-})
+    changeColor();
+});
+
+function changeColor() {
+    if ( counter < 0 ) {
+        counterNum.classList.add("negative-number");
+    }
+    else if ( counter > 0 ) {
+        counterNum.classList.add("positive-number");
+    }
+    else {
+        counterNum.classList.remove("positive-number");
+        counterNum.classList.remove("negative-number");
+    }
+}
+
+magicBtn.addEventListener("click", function() {
+   let askMessage = parseInt( prompt("Choose a number you want to start count from:") );
+   counter = askMessage;
+   counterNum.innerText = counter;
+});

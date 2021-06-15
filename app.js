ac7c7e5600daea666917e55bd55e5a53e48df0ae
Leonardo Boccali <leonardoboccali19@gmail.com>
2021-06-15 09:03:26 +0200

    gived to counterNum the property of '.innerTex = 0'so it display 0 number in the counter

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-let counterNum = document.getElementById("num-showed");
 
-let add = document.getElementById("add-btn");
-let subtract = document.getElementById("subtract-btn");
+
+
+let counterNum = document.getElementById("num-showed");
+counterNum.innerText = 0;
+
+// let add = document.getElementById("add-btn");
+// let subtract = document.getElementById("subtract-btn");
 let resetBtn = document.getElementById("reset-btn");
 let magicBtn = document.getElementById("magicButton-btn");
 
 let counter = 0;
 
+/*
 add.addEventListener("click", function() {
     counter ++;
     counterNum.innerText = counter;
     changeColor();
 });
-
+*/
+/*
 subtract.addEventListener("click", function() {
     counter --;
     counterNum.innerText = counter;
     changeColor();
 });
-
+*/
+/*
 resetBtn.addEventListener("click", function() {
     counter = 0;
-    counterNum.innerText = counter;
+    // counterNum.innerText = counter;
     changeColor();
 });
-
+*/
+/*
 function changeColor() {
     if ( counter < 0 ) {
         counterNum.classList.add("negative-number");
@@ -37,11 +45,12 @@ function changeColor() {
         counterNum.classList.remove("negative-number");
     }
 }
-
+*/
+/*
 magicBtn.addEventListener("click", function() {
    let askMessage = parseInt( prompt("Choose a number you want to start count from:") );
    counter = askMessage;
-   counterNum.innerText = counter;
+   // counterNum.innerText = counter;
    changeColor();
 });
-
+*/
